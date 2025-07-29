e57f50f9734fd566842f42f97a8212ec8af38cc7
omar898 <omar.badawi.rm2024@gmail.com>
2025-07-29 01:42:05 +0000

    All Changes of 7/29/2025

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -5,13 +5,17 @@ class NotesListView extends StatelessWidget {
   const NotesListView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 6),
-          child: CustomNoteItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 6),
+            child: CustomNoteItem(),
+          );
+        },
+      ),
     );
   }
 }
